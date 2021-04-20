85421c96193ab9cee632c9ee1942249fece32ff1
Juerg Haefliger <juergh@canonical.com>
2021-04-20 08:21:03 +0200

    lib.sh: More simplifications
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- lib.sh
+++ lib.sh
@@ -6,67 +6,35 @@
 # Set globals
 PATH=../bottle-pi:${PATH}
 
-function usage()
-{
-	local cmd=${1}
-	local args
-
-	args=
-	cat <<EOF
-Usage: $(basename "${0}") [-l {0,1,2,3,4}] [-q] \
-$(target-pi-cli foo bar "${cmd}" -h | head -1 | sed "s,.* ${cmd} ,,")
-
-$(target-pi-cli foo bar ${cmd} -h | tail -n +3)
-  -l {0,1,2,3,4}, --log-level {0,1,2,3,4}
-                        Set the logging level (0: critical, 1: error, \
-2: warning, 3: info, 4: debug), default: 3
-  -q, --quiet           Only show criticals and errors (equivalent to \
---log-level=1)
-EOF
-}
-
 function run_parallel()
 {
-	local cmd=${1}
-	shift
+	local args
 
-	if [ "${HELP}" -eq 1 ] ; then
-		usage "${cmd}"
-		exit
-	fi
+	args=()
+	while [ ${#} -gt 0 ] ; do
+		case "${1}" in
+			--)
+				shift
+				break
+				;;
+			-*)
+				args+=("${1}")
+				;;
+			*)
+				echo "Invalid argument: ${1}" >&2
+				exit 1
+				;;
+		esac
+		shift
+	done
+
 	# Run the command in parallel on the provided hosts
 	printf "%s\n" "${HOSTS[@]}" | \
-		./run-parallel -- target-pi-cli "${ARGS[@]}" ozzy:5000 {} "${cmd}" \
-					   "${SUB_ARGS[@]}" "${@}"
+		./run-parallel "${args[@]}" -- target-pi-cli ozzy:5000 {} "${@}"
 }
 
-# Parse the commandline arguments
-HELP=0
-ARGS=()
-SUB_ARGS=()
-while [ ${#} -gt 0 ] ; do
-	case "${1}" in
-		-h|--help)
-			HELP=1
-			;;
-
-		# target-pi-cli arguments
-		-l|--log-level)
-			ARGS+=("${1}")
-			shift
-			ARGS+=("${1}")
-			;;
-		-q|--quiet)
-			ARGS+=("${1}")
-			;;
-
-		# target-pi-cli sub-command arguments
-		*)
-			SUB_ARGS+=("${1}")
-			;;
-	esac
-	shift
-done
+# -----------------------------------------------------------------------------
+# Main entry point
 
 if [ -t 0 ] ; then
 	echo "Unable to read hostname(s) from standard input" >&2
